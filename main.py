272e802801109a82091eeaf095df560f854bf874
Navid Asadi <navidreza.asadi@gmail.com>
2021-02-13 01:13:58 +0330

    Titlify prof's names

--- main.py
+++ main.py
@@ -10,43 +10,30 @@ from sanic import response
 import utils
 
 app = Sanic("Professors")
-S = requests.Session()
+app.static('/styles', './styles')
 
 google_base_link = 'https://www.google.com/search?q=%s+%s&ie=UTF-8'
 scholar_base_link = 'https://scholar.google.com/citations?hl=en&view_op=search_authors&mauthors=%s+%s'
-university_logo_base_link = 'https://www.google.com/search?q=university+%s+logo+wikipedia&sclient=img&tbm=isch'
 prof_auto_profile_pic = 'https://www.google.com/search?q=%s+university+%s+profile&sclient=img&tbm=isch'
 
-styles = '<link rel="stylesheet" href="styles/w3.css">'\
-         '<link rel="stylesheet" href="styles/style.css">'
-
-base_row = '''<tr>
-    <td>%s &nbsp; &nbsp;</td>
-    <td><a target="_blank" href="%s">%s Auto</a>&nbsp; &nbsp;</td>
-    <td><a target="_blank" href="%s">Scholar</a>&nbsp; &nbsp;</td>
-    <td><a target="_blank" href="%s">Search</a></td>
-    </tr>'''
-
+Sess = requests.Session()
 
 @app.route("/")
 async def root(request):
     universities = [university.replace('.txt', '').strip() for university in os.listdir('lists/')]
     items = list()
     for university in universities:
-        uni_logo_page = S.get(university_logo_base_link % university)
-        soup = BeautifulSoup(uni_logo_page.text, 'html.parser')
-        uni_logo = soup.find_all('img')[1].attrs['src']
-
-        img_tag = utils.make_img_tag(uni_logo)
+        uni_logo = utils.get_university_logo(Sess, university)
+        logo_tag = utils.make_img_tag(uni_logo, classes="w3-margin-left w3-padding-left")
         href_tag = utils.make_link_tag(quote(university), utils.titlify(university))
-        items.append(img_tag + '&nbsp;' + href_tag + '<br>')
+        items.append(logo_tag + '&nbsp;' + href_tag + '<br>')
 
     # head_tag = globals()['head_tag'] % "Universities"
     # heading = globals()['heading'] % "Universities"
     # header = globals()['header'] % heading
     uni_list = '<div class="w3-container">%s</div>' % '\n'.join(items)
     # return response.html(f'<html>%s<body>%s %s</body></html>' % (head_tag, header, uni_list))
-    return response.html(utils.htmlify("Universities", uni_list))
+    return response.html(utils.htmlify("Universities", "Universities", uni_list))
 
 @app.route("/favicon.ico")
 async def favicon(request):
@@ -56,41 +43,55 @@ async def favicon(request):
 async def list_professors(request, university):
     items = list()
     professors = set()
+
     with open('lists/%s.txt' % university, 'r') as f:
         for i, line in enumerate(f):
             name = line.strip()
             if not name:
-                items.append('</table><hr><table>') # </table><hr><table>
+                items.append({'break':True}) # </table><hr><table>
                 continue
             if name in professors or name.startswith('#'):
                 continue
             professors.add(name)
 
-            professor_pic_page = S.get(prof_auto_profile_pic % (name, university))
+            name = utils.titlify(name)
+            professor_pic_page = Sess.get(prof_auto_profile_pic % (name, university))
             soup = BeautifulSoup(professor_pic_page.text, 'html.parser')
             professor_pic = soup.find_all('img')[1].attrs['src']
-            img_tag = utils.make_img_tag(professor_pic)
-
+            img_tag = utils.make_img_tag(professor_pic, (100, 120), "w3-round")
             name_quoted = quote(name)
             university_quoted = quote(university)
-            auto = '%s/%s' % (university_quoted, name_quoted)
-            row = base_row % (name, auto, img_tag, scholar_base_link % (name_quoted, university_quoted), google_base_link % (name_quoted, university_quoted))
-            items.append(row)
+            auto = "%s/%s" % (university_quoted, name_quoted)
+            # row = base_row % (name, auto, img_tag, scholar_base_link % (name_quoted, university_quoted), google_base_link % (name_quoted, university_quoted))
+            # items.append(row)
 
-            content = "<table>%s</table>" % '\n'.join(items)
-    # head_tag = globals()['head_tag'] % utils.titlify(university)
-    # heading = globals()['heading'] % utils.titlify(university)
-    # header = globals()['header'] % heading
-    # return response.html('<html>%s<body>%s<table>%s</table></body></html>' % (head_tag, header, '\n'.join(items)))
-    return response.html(utils.htmlify(utils.titlify(university), content))
+            # content = "<table>%s</table>" % '\n'.join(items)
+
+            items.append({
+                'break': False,
+                'photo': img_tag,
+                'name': name,
+                'auto': auto,
+                'scholar': scholar_base_link % (name_quoted, university_quoted),
+                'search': google_base_link % (name_quoted, university_quoted)
+            })
+
+        content = utils.professors_info(items)
+
+    uni_logo = utils.get_university_logo(Sess, university)
+    logo_tag = utils.make_img_tag(uni_logo, (70, 70), classes="w3-margin-right w3-padding-right")
+    heading = logo_tag + utils.titlify(university)
+    heading = utils.make_link_tag(quote("/"), heading, "w3-text-red")
+    header = utils.titlify(university)
+    return response.html(utils.htmlify(header, heading, content))
 
 
 @app.route("/<university>/<name>")
 async def redirect_to_prof_page(request, university, name):
     university_quoted, name_quoted = quote(university), quote(name)
     scholar = scholar_base_link % (name_quoted, university_quoted)
-    # html_page = urllib.request.urlopen(scholar)
-    html_page = S.get(scholar)
+
+    html_page = Sess.get(scholar)
     soup = BeautifulSoup(html_page.text, 'html.parser')
     links = soup.select('a.gs_ai_pho')
     if len(links) == 0:
