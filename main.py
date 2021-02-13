72bedcec920ff73025bf9abafa9a8f5f2dc0c4f6
Sina <sina.hajimiri@gmail.com>
2021-02-13 13:35:56 +0330

    Add generate all feature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from time import sleep
 from urllib.parse import quote
 import urllib.request
 
@@ -32,6 +33,7 @@ async def favicon(request):
 
 @app.route("/<university>")
 async def list_professors(request, university):
+    generate_all = request.args.get('generateall', '').lower() == 'true'
     items = list()
     professors = set()
     with open('lists/%s.txt' % university, 'r') as f:
@@ -40,12 +42,19 @@ async def list_professors(request, university):
             if not name:
                 items.append('</table><hr><table>')
                 continue
+            elif name.startswith('#'):
+                items.append('<b>%s</b>' % name[1:])
             if name in professors or name.startswith('#'):
                 continue
             professors.add(name)
             name_quoted = quote(name)
             university_quoted = quote(university)
-            auto = '%s/%s' % (university_quoted, name_quoted)
+            if generate_all:
+                auto = await find_auto_value(university, name)
+                if i % 10 == 9:
+                    sleep(5)
+            else:
+                auto = '%s/%s' % (university_quoted, name_quoted)
             row = base_row % (name, auto, scholar_base_link % (name_quoted, university_quoted), google_base_link % (name_quoted, university_quoted))
             items.append(row)
     return response.html('<html><body><h3>%s</h3>\n<table>%s</table></body></html>' % (university, '\n'.join(items)))
@@ -53,18 +62,22 @@ async def list_professors(request, university):
 
 @app.route("/<university>/<name>")
 async def redirect_to_prof_page(request, university, name):
+    return response.redirect(await find_auto_value(university, name))
+
+
+async def find_auto_value(university, name):
     university_quoted, name_quoted = quote(university), quote(name)
     scholar = scholar_base_link % (name_quoted, university_quoted)
     html_page = urllib.request.urlopen(scholar)
     soup = BeautifulSoup(html_page, 'html.parser')
     links = soup.select('a.gs_ai_pho')
     if len(links) == 0:
-        google = google_base_link % (name_quoted, university_quoted)
-        return response.redirect(google)
+        redirect = google_base_link % (name_quoted, university_quoted)
     elif len(links) > 1:
-        return response.redirect(scholar)
+        redirect = scholar
     else:
-        return response.redirect('https://scholar.google.com%s&view_op=list_works&sortby=pubdate' % links[0].get('href'))
+        redirect = 'https://scholar.google.com%s&view_op=list_works&sortby=pubdate' % links[0].get('href')
+    return redirect
 
 
 if __name__ == "__main__":
